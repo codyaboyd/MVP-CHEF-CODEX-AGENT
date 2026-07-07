b541b246b448396fdfe0aae086b542d349952cdf
BitCloud_ <57097960+codyaboyd@users.noreply.github.com>
2026-07-07 02:14:43 -0400

    Build Bootstrap recipe dashboard UI

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express');
+const pageController = require('../controllers/pageController');
 const recipeController = require('../controllers/recipeController');
 
 const router = express.Router();
 
-router.get('/', recipeController.home);
+router.get('/', pageController.dashboard);
+router.get('/projects', pageController.projects);
+router.get('/recipes', pageController.recipes);
 router.get('/recipes/new', recipeController.newRecipeForm);
 router.post('/recipes', recipeController.createRecipe);
 router.get('/recipes/:id', recipeController.showRecipe);
+router.get('/runs/:id', pageController.runDetail);
+router.get('/settings', pageController.settings);
 
 module.exports = router;
